dd9b29bf4994b38b5024dc16904d2ea772483f30
Charlie Chiccarine <src322@drexel.edu>
2019-12-29 01:05:27 -0500

    Finished Day7P2

--- Day07/day7p2.py
+++ Day07/day7p2.py
@@ -5,237 +5,236 @@
 # 
 # Author : Charlie Rose
 # Language : Python3
-# Date : INCOMPLETE
+# Date : 12/29/2019
+
+###############################################################################
 
 from itertools import permutations
 
-def day7main(in1, in2):
-    f = open("day7input.txt")
+prog = 0
+programHC = []
+programs = [ [], [], [], [], [] ]
+ips = [0, 0, 0, 0, 0]
 
-    # read the file and prep it for parsing
-    program = f.readlines()[0].split(",")
-    for i in range(0, len(program)):
-        program[i] = int(program[i])
+###############################################################################
 
-    inputOn = 1
+def getOperand(op, mode):
+    rv = -1
+    if mode == 1:
+        # Immidiate Mode
+        rv =  op
+    elif mode == 0:
+        # Destination Mode
+        rv =  accessMemory(op)
+    else:
+        print( "Bad Mode" )
+        return -1
+    return rv
 
-    i = 0
-    while i < len(program):
-        if program[i] > 99:
-            longopcode = str(program[i])
+def increaseMemory(mem):
+    global programs
 
-            while len(longopcode) < 5:
-                longopcode = "0" + longopcode
+    while len(programs[prog]) <= mem:
+        programs[prog].append(0)
 
-            opcode = int(longopcode[3:5])
-            param1mode = int(longopcode[2])
-            param2mode = int(longopcode[1])
-            param3mode = int(longopcode[0])
+def accessMemory(loc):
+    if loc >= len(programs[prog]):
+        increaseMemory(loc)
+    return programs[prog][loc]
+
+def editMemory(loc, val):
+    global programs
+
+    if loc >= len(programs[prog]):
+        increaseMemory(loc)
+    programs[prog][loc] = val
 
 
-        else:
-            opcode = program[i]
-            param1mode = 0
-            param2mode = 0
-            param3mode = 0
+# Function : Run Program
+# Parameters : 
+#   none
+# Returns :
+#   Halt Code ( "HALT" )
+def runProgram( in1, in2  ): 
+    global ips
+    if in1 == 0:
+        inputMode = 2
+    else:
+        inputMode = 1
+    
+    ip = ips[prog]
+
+    while ip < len(programs[prog]):
+        longopcode = str(accessMemory(ip))
+
+        while len(longopcode) < 5:
+            longopcode = "0" + longopcode
+
+        opcode = int(longopcode[3:5])
+        param1mode = int(longopcode[2])
+        param2mode = int(longopcode[1])
+        param3mode = int(longopcode[0])
 
         # Opcode 1 : Add
         if opcode == 1:
-            operand1 = program[i + 1]
-            operand2 = program[i + 2]
-            dest = program[i + 3]
+            operand1 = accessMemory(ip + 1)
+            operand2 = accessMemory(ip + 2)
+            operand3 = accessMemory(ip + 3)
 
-            if param1mode:
-                # Immidiate Mode
-                val1 = operand1
-            else:
-                # Destination Mode
-                val1 = program[operand1]
+            val1 = getOperand(operand1, param1mode)
+            val2 = getOperand(operand2, param2mode)
+            dest = operand3
 
-            if param2mode:
-                # Immidiate Mode
-                val2 = operand2
-            else:
-                # Destination Mode
-                val2 = program[operand2]
+            editMemory(dest, val1 + val2)
 
-            program[dest] = val1 + val2 
-
-            i += 4
+            ip += 4
 
         # Opcode 2 : Multiply
         if opcode == 2:
-            operand1 = program[i + 1]
-            operand2 = program[i + 2]
-            dest = program[i + 3]
+            operand1 = accessMemory(ip + 1)
+            operand2 = accessMemory(ip + 2)
+            operand3 = accessMemory(ip + 3)
  
-            if param1mode:
-                # Immidiate Mode
-                val1 = operand1
-            else:
-                # Destination Mode
-                val1 = program[operand1]
+            val1 = getOperand(operand1, param1mode)
+            val2 = getOperand(operand2, param2mode)
+            dest = operand3
 
-            if param2mode:
-                # Immidiate Mode
-                val2 = operand2
-            else:
-                # Destination Mode
-                val2 = program[operand2]
-
-            program[dest] = val1 * val2 
+            editMemory(dest, val1 * val2) 
             
-            i += 4
+            ip += 4
     
         # Opcode 3 : Get Input
         if opcode == 3:
-            dest = program[i + 1]
+            operand1 = accessMemory(ip + 1)
 
-            if inputOn == 1:
+            dest = operand1
+
+            if inputMode == 1:
                 value = in1
-                inputOn = 2
-            elif inputOn == 2:
+                inputMode = 2
+            else:
                 value = in2
 
-            program[dest] = value
+            editMemory(dest, value)
     
-            i += 2 
+            ip += 2 
     
         # Opcode 4 : Give Output
         if opcode == 4:
-            operand1 = program[i + 1]
+            operand1 = accessMemory(ip + 1)
   
-            if param1mode:
-                # Immidiate Mode
-                outVal = operand1
-            else:
-                # Destination Mode
-                outVal = program[operand1]
+            outVal = getOperand(operand1, param1mode)
 
-            return outVal 
-
-            i += 2
+            ip += 2
+            
+            ips[prog] = ip
+            return outVal
 
         # Opcode 5 : Jump If True
         if opcode == 5:
-            operand1 = program[i + 1]
-            operand2 = program[i + 2]
+            operand1 = accessMemory(ip + 1)
+            operand2 = accessMemory(ip + 2)
  
-            if param1mode:
-                # Immidiate Mode
-                test = operand1
-            else:
-                # Destination Mode
-                test = program[operand1]
-
-            if param2mode:
-                # Immidiate Mode
-                dest = operand2
-            else:
-                # Destination Mode
-                dest = program[operand2]
+            test = getOperand(operand1, param1mode)
+            dest = getOperand(operand2, param2mode)
 
             if test:
-                i = dest
+                ip = dest
             else:
-                i += 3
+                ip += 3
 
         # Opcode 6 : Jump If False
         if opcode == 6:
-            operand1 = program[i + 1]
-            operand2 = program[i + 2]
-
-            if param1mode:
-                # Immidiate Mode
-                test = operand1
-            else:
-                # Destination Mode
-                test = program[operand1]
-
-            if param2mode:
-                # Immidiate Mode
-                dest = operand2
-            else:
-                # Destination Mode
-                dest = program[operand2]
+            operand1 = accessMemory(ip + 1)
+            operand2 = accessMemory(ip + 2)
+        
+            test = getOperand(operand1, param1mode)
+            dest = getOperand(operand2, param2mode)
             
             if (not test):
-                i = dest
+                ip = dest
             else:
-                i += 3
+                ip += 3
 
         # Opcode 7 : Less Than
         if opcode == 7:
-            operand1 = program[i + 1]
-            operand2 = program[i + 2]
-            operand3 = program[i + 3]
+            operand1 = accessMemory(ip + 1)
+            operand2 = accessMemory(ip + 2)
+            operand3 = accessMemory(ip + 3)
 
-            if param1mode:
-                # Immidiate Mode
-                val1 = operand1
-            else:
-                # Destination Mode
-                val1 = program[operand1]
+            val1 = getOperand(operand1, param1mode)
+            val2 = getOperand(operand2, param2mode)
+            dest = operand3
 
-            if param2mode:
-                # Immidiate Mode
-                val2 = operand2
-            else:
-                # Destination Mode
-                val2 = program[operand2]
+            editMemory(dest, int( val1 < val2 ))
 
-            program[operand3] = int( val1 < val2 )
-
-            i += 4
+            ip += 4
             
 
         # Opcode 8 : Equals
         if opcode == 8:
-            operand1 = program[i + 1]
-            operand2 = program[i + 2]
-            operand3 = program[i + 3]
+            operand1 = accessMemory(ip + 1)
+            operand2 = accessMemory(ip + 2)
+            operand3 = accessMemory(ip + 3)
+        
+            val1 = getOperand(operand1, param1mode)
+            val2 = getOperand(operand2, param2mode)
+            dest = operand3
 
-            if param1mode:
-                # Immidiate Mode
-                val1 = operand1
-            else:
-                # Destination Mode
-                val1 = program[operand1]
+            editMemory(dest, int( val1 == val2 ))
 
-            if param2mode:
-                # Immidiate Mode
-                val2 = operand2
-            else:
-                # Destination Mode
-                val2 = program[operand2]
+            ip += 4
 
-            program[operand3] = int( val1 == val2 )
-
-            i += 4
- 
         # Opcode 99 : Halt
         if opcode == 99:
-            break
+            return "HALT"
 
-    f.close()
+###############################################################################
 
 
 if __name__ == "__main__":
-    sequences = [''.join(p) for p in permutations("56789")]
+    f = open("day7input.txt")
+    # Load programs
+    programf = f.readlines()[0].split(",")
+    for i in range(0, len(programf)):
+        programHC.append( int(programf[i]) )
     
-    maxSeq = sequences[0]
-    carry = 0
-    for v in maxSeq:
-        carry = day7main(int(v), carry)
-    maxVal = carry
+    # Actually run the day 7
+    sequences = [''.join(p) for p in permutations("56789")]
+
+    maxSeq = ""
+    maxVal = 0
     
     for seq in sequences:
+        ips = [0,0,0,0,0]
+        programs = [ [], [], [], [], []]
+        for p in range(5):
+            for n in programHC:
+                programs[p].append(n)
+
         carry = 0
         for v in seq:
-            carry = day7main(int(v), carry)
+            carry = runProgram(int(v), carry)
+            prog += 1
+            prog = prog % 5
+
+        while True:
+            cT = runProgram(0, carry)
+            prog += 1
+            prog = prog % 5
+            if not(cT == "HALT"):
+                carry = cT
+            else:
+                break
+
 
         if carry > maxVal:
             maxVal = carry
             maxSeq = seq
 
+        print( seq, ":", carry)
+            
     print("Sequence :", maxSeq, ", Value :", maxVal)
+
+    f.close()
+
