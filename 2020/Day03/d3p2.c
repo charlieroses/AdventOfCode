dc7a8eb937d3b4df53344d1014d3120d22d144a7
Charlie <src322@drexel.edu>
2020-12-03 12:15:17 -0500

    Part 2 the right way

--- 2020/Day03/d3p2.c
+++ 2020/Day03/d3p2.c
@@ -2,34 +2,49 @@
 #include <stdlib.h>
 
 int main() {
+	FILE *fp;
 	char s[256];
-	int slen;
+	int slen, height;
 	int i, j, k, d, r, rcurr, tree;
+	long treemult;
+	int coords[5][2] = {{1, 1}, {1, 3}, {1, 5}, {1, 7}, {2, 1}};
 
-	i = 0;
-	j = 0;
-	d = 1;
-	r = 7;
+	fp = fopen("input.txt", "r");
 
-	slen = 0;
-	tree = 0;
-	while( scanf("%s\n", s) != EOF ) {
-		if( i == 0 ) {
-			for(k = 0; s[k] != 0; k++) {
-				slen += 1;
+	treemult = 1;
+	for(i = 0; i < 5; i++) {
+		slen = 0;
+		tree = 0;
+		height = 0;
+		j = 0;
+
+		d = coords[i][0];
+		r = coords[i][1];
+
+		while( fscanf(fp, "%s\n", s) != EOF ) {
+			if( height == 0 ) {
+				for(k = 0; s[k] != 0; k++) {
+					slen += 1;
+				}
 			}
+
+			if( height % d == 0) {
+				if( s[j % slen] == '#') {
+					tree++;
+				}
+
+				j += r;
+			}
+
+			height += 1;
 		}
 
-		if( i % d == 0) {
-			if( s[j % slen] == '#') {
-				tree++;
-			}
-
-			j += r;
-		}
-
-		i += 1;
+		treemult *= tree;
+		printf("d=%d, r=%d, trees=%d \n", d, r, tree);
+		rewind(fp);
 	}
 
-	printf("%d \n", tree);
+	printf("%ld\n", treemult);
+
+	fclose(fp);
 }
